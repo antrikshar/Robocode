67f1d4a3a091eab0beca9cf9b40748f2c64e6804
antrikshar <74371851+antrikshar@users.noreply.github.com>
2021-11-04 17:52:23 +0100

    robot for battle 3
    Version 3.0

--- antriksh.java
+++ antriksh.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 /*
- * antriksh 2.0
+ * antriksh 3.0
  *
  * By Antriksh Arora
  */
@@ -19,7 +19,6 @@ public class antriksh extends AdvancedRobot {
     boolean closeToWall; // is true when robot is closer than 50px to any wall
     int others; // saves the number enemies at beginning of the fight
     int missedBullets = 0; // counts the streak in which bullets don't hit any enemy
-    int counter = 0; // counts whether bullet should be shot or not after a long miss streak
 
     public void run() {
 
@@ -47,6 +46,9 @@ public class antriksh extends AdvancedRobot {
         forwardMovement = true;
         setTurnRadarRight(360); // scans around himself to spot enemies
 
+
+
+
         while (true) {
 
             // if the robot was close a wall, check if it has moved away yet and set the variable to false
@@ -70,6 +72,7 @@ public class antriksh extends AdvancedRobot {
             }
 
             execute();
+
         }
     }
 
@@ -81,13 +84,24 @@ public class antriksh extends AdvancedRobot {
         double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
         double bearingFromRadar = normalRelativeAngleDegrees(absoluteBearing - getRadarHeading());
 
-        // movement: move around enemy in circle
-        if (forwardMovement){
-            setTurnRight(normalRelativeAngleDegrees(event.getBearing() + 80));
-        }
-        // if is moving backwards, add 100 - 80 and 100 causes that robot moves a bit closer after hitting wall
-        else {
-            setTurnRight(normalRelativeAngleDegrees(event.getBearing() + 100));
+
+        if (getOthers() / (double) others > 0.5) {
+            if (forwardMovement) {
+                setTurnRight(normalRelativeAngleDegrees(event.getBearing()));
+            }
+            // if is moving backwards, add 100 - 80 and 100 causes that robot moves a bit closer after hitting wall
+            else {
+                setTurnRight(normalRelativeAngleDegrees(event.getBearing()));
+            }
+        } else {
+            // movement: move around enemy in circle
+            if (forwardMovement) {
+                setTurnRight(normalRelativeAngleDegrees(event.getBearing() + 100));
+            }
+            // if is moving backwards, add 100 - 80 and 100 causes that robot moves a bit closer after hitting wall
+            else {
+                setTurnRight(normalRelativeAngleDegrees(event.getBearing() + 120));
+            }
         }
 
         setTurnGunRight(bearingFromGun); // turns gun to keep aim at enemy
@@ -96,56 +110,85 @@ public class antriksh extends AdvancedRobot {
         // if the missed bullet steak is lower than 10
         if (missedBullets <= 10) {
 
+            setTurnGunRight(bearingFromGun + (event.getVelocity()/10)*(event.getDistance()/10)/2);
             // until 6 out of 8 enemy are alive fire with max power bullets
             // check gun heat and energy to not kill yourself
-            if (getOthers() / (double) others > 0.75 && getGunHeat() == 0 && getEnergy() > 15) {
+            if (getOthers() / (double) others > 0.5 && getGunHeat() == 0 && getEnergy() > 15) {
                 fire(3);
-            }
-            // until 5 out of 8 enemy are alive fire with power of 2.5
-            else if (getOthers() / (double) others > 0.625 && getGunHeat() == 0 && getEnergy() > 15) {
-                fire(2.5);
-            }
-            // until 4 out of 8 enemy are alive fire with power of 2.25
-            else if (getOthers() / (double) others > 0.5 && getGunHeat() == 0 && getEnergy() > 15) {
-                fire(2.25);
+            } else if (getOthers() / (double) others > 0.375 && getGunHeat() == 0 && getEnergy() > 15) {
 
-            }
-            // setting conditions to select the power of the bullet
-            else if (event.getDistance() > 200 && getGunHeat() == 0 && getEnergy() > 5) {
-                fire(1);
+                // setting conditions to select the power of the bullet
+                if (event.getDistance() > 200 && getGunHeat() == 0 && getEnergy() > 5) {
+                    fire(1.5);
 
-            } else if (event.getDistance() > 150 && getGunHeat() == 0 && getEnergy() > 15) {
-                fire(1.5);
+                } else if (event.getDistance() > 150 && getGunHeat() == 0 && getEnergy() > 15) {
+                    fire(2);
 
-            } else if (event.getDistance() > 100 && getGunHeat() == 0 && getEnergy() > 15) {
-                fire(2);
+                } else if (event.getDistance() > 100 && getGunHeat() == 0 && getEnergy() > 15) {
+                    fire(2.5);
 
-            } else if (event.getDistance() > 50 && getGunHeat() == 0 && getEnergy() > 10) {
-                fire(2.5);
-
-            } else if (event.getDistance() < 50 && getGunHeat() == 0) {
-                if (event.getEnergy() < 3.1) {
-                    fire(1);
-                } else {
+                } else if (event.getDistance() > 50 && getGunHeat() == 0 && getEnergy() > 10) {
                     fire(3);
+
+                } else if (event.getDistance() < 50 && getGunHeat() == 0) {
+                    fire(3);
+                } else {
+                    // else just keep track the enemy
+                    setTurnGunRight(bearingFromGun);
+                    setTurnRadarRight(bearingFromRadar);
                 }
-            } else {
-                // else just keep track the enemy
-                setTurnGunRight(bearingFromGun);
-                setTurnRadarRight(bearingFromRadar);
+            } else if (getOthers() / (double) others > 0.25 && getGunHeat() == 0 && getEnergy() > 15) {
+
+                // setting conditions to select the power of the bullet
+                if (event.getDistance() > 200 && getGunHeat() == 0 && getEnergy() > 5) {
+                    fire(1.5);
+
+                } else if (event.getDistance() > 150 && getGunHeat() == 0 && getEnergy() > 15) {
+                    fire(2);
+
+                } else if (event.getDistance() > 100 && getGunHeat() == 0 && getEnergy() > 15) {
+                    fire(2.5);
+
+                } else if (event.getDistance() > 50 && getGunHeat() == 0 && getEnergy() > 10) {
+                    fire(3);
+
+                } else if (event.getDistance() < 50 && getGunHeat() == 0) {
+                    fire(3);
+                } else {
+                    // else just keep track the enemy
+                    setTurnGunRight(bearingFromGun);
+                    setTurnRadarRight(bearingFromRadar);
+                }
+            } else if (getOthers() / (double) others > 0.125 && getGunHeat() == 0 && getEnergy() > 15) {
+
+                // setting conditions to select the power of the bullet
+                if (event.getDistance() > 200 && getGunHeat() == 0 && getEnergy() > 5) {
+                    fire(1);
+
+                } else if (event.getDistance() > 150 && getGunHeat() == 0 && getEnergy() > 15) {
+                    fire(2.5);
+
+                } else if (event.getDistance() > 100 && getGunHeat() == 0 && getEnergy() > 15) {
+                    fire(2);
+
+                } else if (event.getDistance() > 50 && getGunHeat() == 0 && getEnergy() > 10) {
+                    fire(2.5);
+
+                } else if (event.getDistance() < 50 && getGunHeat() == 0) {
+                    fire(3);
+                } else {
+                    // else just keep track the enemy
+                    setTurnGunRight(bearingFromGun);
+                    setTurnRadarRight(bearingFromRadar);
+                }
             }
         }
         // when the missed bulled steak is above 10, then fire only every third bullet
-        else if (counter %3 == 0) {
-            fire(1);
-            counter++;
-        } else {
-            // else just keep track the enemy and raise the counter
-            setTurnGunRight(bearingFromGun);
-            setTurnRadarRight(bearingFromRadar);
-            counter++;
+        else {
+            fire(0.1);
         }
     }
+
 
     // in case the robot still touches the wall
     public void onHitWall(HitWallEvent event) {
@@ -162,29 +205,22 @@ public class antriksh extends AdvancedRobot {
         }
     }
 
-    public void onHitByBullet(HitByBulletEvent event) {
-        // if robot gets hit it does sharper turns
-        if (forwardMovement){
-            setTurnRight(normalRelativeAngleDegrees(event.getBearing() -20));
-        }
-        else {
-            setTurnRight(normalRelativeAngleDegrees(event.getBearing() + 20));
-        }
-    }
-
 
     public void onHitRobot(HitRobotEvent event) {
+
         // if we drive into another robot, then bounce - when another robot drives into us, do nothing
         if (event.isMyFault()) {
             bounce();
         }
     }
 
+    public void onHitByBullet(HitByBulletEvent event) {
+        missedBullets = 0;
+    }
 
     public void onBulletHit(BulletHitEvent event) {
         // reset the missed bullets streak
         missedBullets = 0;
-        counter = 0;
     }
 
 
@@ -193,15 +229,4 @@ public class antriksh extends AdvancedRobot {
         missedBullets++;
     }
 
-
-    public void onWin(WinEvent event) {
-        // do a win dance
-        for (int i = 0; i < 50; i++) {
-            turnRight(30);
-            turnLeft(30);
-        }
-    }
-
-
-    // TODO: predict the enemies movement to get better aim and miss lesser bullets
 }
